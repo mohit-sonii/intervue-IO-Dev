7682d68c5142817de2f7e3bd81488c3f6bd142eb
mohit-sonii <mohitsoni9731@gmail.com>
2025-06-07 14:15:25 +0530

    Deploying Socket IO -> Updated the Connection URL in app.js in Backend folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
    cors: {
-      origin: "http://localhost:5173",
+      origin: "https://intervue-io-dev.vercel.app",
       methods: ["GET", "POST"],
       credentials: true
    },
